7ace3edb656481933038d59bc3d9b1ed6d2cb0f9
narumincho <16481886+narumincho@users.noreply.github.com>
2023-04-21 16:20:25 +0900

    add test

---
+++ packages/narumincho_util/test/test.dart
@@ -0,0 +1,38 @@
+import 'package:fast_immutable_collections/fast_immutable_collections.dart';
+import 'package:narumincho_util/narumincho_util.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('flatMapAndRemoveNull', () {
+    expect(
+      const IListConst(['a', 'bb', 'c']).flatMapAndRemoveNull((text) {
+        if (text.length == 2) {
+          return [null];
+        }
+        return [text, text * 2];
+      }),
+      const IListConst(['a', 'aa', 'c', 'cc']),
+    );
+  });
+
+  test('splitOverlappingTuple2', () {
+    expect(
+      splitOverlappingTuple2(['a', 'b', 'c']),
+      const IListConst([Tuple2('a', 'b'), Tuple2('b', 'c')]),
+    );
+  });
+
+  test('splitOverlappingTuple2 empty', () {
+    expect(
+      splitOverlappingTuple2([]),
+      const IListConst([]),
+    );
+  });
+
+  test('splitOverlappingTuple2 1', () {
+    expect(
+      splitOverlappingTuple2(['a']),
+      const IListConst([]),
+    );
+  });
+}
