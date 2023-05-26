2ca3faf5557dc0fda27c84104e9e19711825ee95
narumincho <16481886+narumincho@users.noreply.github.com>
2023-05-26 09:12:00 +0000

    add NonEmptyString

--- packages/narumincho_util/lib/narumincho_util.dart
+++ packages/narumincho_util/lib/narumincho_util.dart
@@ -132,3 +132,24 @@ Uri uriAbsolute({
     queryParameters: queryParameters?.unlock,
   );
 }
+
+@immutable
+final class NonEmptyString {
+  const NonEmptyString._(this.value);
+
+  static NonEmptyString? fromString(String value) {
+    if (value.isEmpty) {
+      return null;
+    }
+    return NonEmptyString._(value);
+  }
+
+  final String value;
+
+  @override
+  int get hashCode => value.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      other is NonEmptyString && other.value == value;
+}
