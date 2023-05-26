68f7a7c105a20f289544f9ded09ca40d0c791bd0
narumincho <16481886+narumincho@users.noreply.github.com>
2023-05-26 03:08:57 +0000

    Avoid unreachable_switch_case bug

--- packages/simple_graphql_client_gen/lib/query_string.dart
+++ packages/simple_graphql_client_gen/lib/query_string.dart
@@ -153,6 +153,8 @@ String _queryFieldToString(
                   _queryFieldListToStringLoop(objectType, indent),
                 _ => '',
               },
+        // Dart 3.0.2 bug https://github.com/dart-lang/sdk/issues/52151
+        // ignore: unreachable_switch_case
         QueryFieldOn(:final typeName, :final return_) =>
           return_.toFieldList().isEmpty
               ? '# ... on ' + typeName + '{}'
@@ -191,6 +193,8 @@ IList<QueryInputVariable> _collectVariableInQueryField(
         }
         return null;
       }),
+    // Dart 3.0.2 bug https://github.com/dart-lang/sdk/issues/52151
+    // ignore: unreachable_switch_case
     QueryFieldOn(:final return_) => collectVariableInQueryFieldList(return_),
   };
 }
